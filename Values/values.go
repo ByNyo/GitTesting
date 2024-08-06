2be94116025433586a597b91f171cba0e903e38a
Leon Zuelsdorf <leonzuelsdorf@gmail.com>
2024-08-06 15:38:43 +0200

    commit

--- Values/values.go
+++ Values/values.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("go" + "lang")
+	fmt.Println("go" + "lang") // Combines two strings and prints them
 
-	fmt.Println("1+1 =", 1+1)
-	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	fmt.Println("1+1 =", 1+1)         // Shows the "task" and calculates it then prints the "task" with the solution
+	fmt.Println("7.0/3.0 =", 7.0/3.0) // Shows the "task" and calculates it then prints the "task" with the solution
 
-	fmt.Println(true && false)
-	fmt.Println(true || false)
-	fmt.Println(!true)
+	fmt.Println(true && false) // If something is True and Something is False. The answer is false.
+	fmt.Println(true || false) // If something is True (and)or False. The answer is true.
+	fmt.Println(!true)         // If something isn't true. The answer is false.
 }
