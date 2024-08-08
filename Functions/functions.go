c170ee1f1fce6d3342d2a956df83397722254791
Leon Zuelsdorf <leonzuelsdorf@gmail.com>
2024-08-08 10:13:44 +0200

    Functions

--- Functions/functions.go
+++ Functions/functions.go
@@ -2,19 +2,19 @@ package main
 
 import "fmt" // Importing fmt (Format)
 
-func plus(a int, b int) int {
-	return a + b
+func plus(a int, b int) int { // A function that takes two parameters a & b both integers
+	return a + b // Returns the value of a + b
 }
 
-func plusPlus(a, b, c int) int {
-	return a + b + c
+func plusPlus(a, b, c int) int { // A function that takes three parameters a, b & c all integers
+	return a + b + c // Returns the value of a + b +c
 }
 
 func main() {
 
-	res := plus(1, 2)
-	fmt.Println("1+2 =", res)
+	res := plus(1, 2)         // Creates a new int variable called res with the result of the function plus
+	fmt.Println("1+2 =", res) // Prints a string with the int variable res
 
-	res = plusPlus(1, 2, 3)
-	fmt.Println("1+2+3 =", res)
+	res = plusPlus(1, 2, 3)     // Sets the int variable res with the result of the function plusPlus
+	fmt.Println("1+2+3 =", res) // Prints a string with the int variable res
 }
