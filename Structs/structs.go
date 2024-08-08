20629b256a524b1b9293e95b0ebbaabe46207028
Leon Zuelsdorf <leonzuelsdorf@gmail.com>
2024-08-08 11:43:51 +0200

    Comments

--- Structs/structs.go
+++ Structs/structs.go
@@ -2,38 +2,38 @@ package main
 
 import "fmt"
 
-type person struct {
-	name string
-	age  int
+type person struct { // Creates a new struct called person
+	name string // A string that can be set in a variable with the person type
+	age  int    // An int that can be set in a variable with the person type
 }
 
-func newPerson(name string) *person {
-	p := person{name: name}
-	p.age = 42
-	return &p
+func newPerson(name string) *person { // A function that takes a string and return type of person
+	p := person{name: name} // Creates person with name
+	p.age = 42              // Sets the persons age to 42
+	return &p               // Returns person type
 }
 
 func main() {
-	fmt.Println(person{"Bob", 20})
-	fmt.Println(person{name: "Alice", age: 30})
-	fmt.Println(person{name: "Fred"})
-	fmt.Println(&person{name: "Ann", age: 40})
-	fmt.Println(newPerson("Jon"))
-	s := person{name: "Sean", age: 50}
-	fmt.Println(s.name)
+	fmt.Println(person{"Bob", 20})              // Prints a person
+	fmt.Println(person{name: "Alice", age: 30}) // Prints a person
+	fmt.Println(person{name: "Fred"})           // Prints a person
+	fmt.Println(&person{name: "Ann", age: 40})  // Prints a person
+	fmt.Println(newPerson("Jon"))               // Prints a person
+	s := person{name: "Sean", age: 50}          // Prints a person
+	fmt.Println(s.name)                         // Prints a name
 
-	sp := &s
-	fmt.Println(sp.age)
+	sp := &s            // Gets a person from the person variable s
+	fmt.Println(sp.age) // Prints an age
 
-	sp.age = 51
-	fmt.Println(sp.age)
+	sp.age = 51         // Sets the age of person variable sp
+	fmt.Println(sp.age) // Prints an age
 
-	dog := struct {
-		name   string
-		isGood bool
-	}{
-		"Rex",
-		true,
+	dog := struct { // Creates a new struct called dog
+		name   string // A string that can be set in a variable with the dog type
+		isGood bool   // A bool that can be set in a variable with the dog type
+	}{ // Creates a dog
+		"Rex", // Name of the dog
+		true,  // Bool if the dog is good
 	}
-	fmt.Println(dog)
+	fmt.Println(dog) // Prints the dog
 }
